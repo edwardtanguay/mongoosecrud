58ad1b69f691624f96ba426cdd26acff944cc641
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-18 06:33:11 +0100

    try works add

--- try.js
+++ try.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const crudCommand = 'add';
+const crudCommand = 'addx';
 
 mongoose.connect("mongodb://localhost:27017/api001", {
 	useUnifiedTopology: true,
@@ -10,8 +10,8 @@ mongoose.connect("mongodb://localhost:27017/api001", {
 const db = mongoose.connection;
 
 db.once("open", err => {
-	if (err) console.log('ERROR ON DATABASE OPEN');
-	else console.log('database open');
+	if (err) console.log('ERROR ON CONNECTION OPEN');
+	else console.log('connection open');
 
 	const userSchema = mongoose.Schema({
 		name: String,
@@ -27,17 +27,18 @@ db.once("open", err => {
 			user1.save((err, doc) => {
 				if (err) return console.log(err);
 				console.log('user inserted: ' + user1.name);
-				mongoose.connection.close(err => {
-					if (err) console.log('ERROR ON DATABASE CLOSE');
-					else console.log('database closed');
-				});
+				closeConnection();
 			});
 			break;
 		default:
 			console.log('bad comment: ' + crudCommand);
-			mongoose.connection.close(err => {
-				if (err) console.log('ERROR ON DATABASE CLOSE');
-				else console.log('database closed');
-			});
+			closeConnection();
 	}
 });
+
+function closeConnection() {
+	mongoose.connection.close(err => {
+		if (err) console.log('ERROR ON DATABASE CLOSE');
+		else console.log('database closed');
+	});
+}
